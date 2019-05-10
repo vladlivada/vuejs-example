60d3ee8c0aa92cc3f84a7015fcf35b4b78494b50
vlivada <vlad.livada@endava.com>
2019-05-10 15:54:05 +0300

    Initial

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="header">
+        <h1>Todo Project</h1>
+    </header>
+</template>
+
+<script>
+    export default{
+        name:"Header"
+    }
+</script>
+
+<style scoped>
+    h1{
+        text-align:center;
+        background:grey;
+        color:#fff;
+    }
+</style>
